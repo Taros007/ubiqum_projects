adcd33f55dd285199413f8cfa2dd33f179705731
NYX <nyx@tano.bytano.nl>
2019-03-26 16:15:49 +0100

    update week6

---
+++ week6/R/replacer.R
@@ -0,0 +1,87 @@
+df <- readr::read_csv('./input/ElectronidexTransactions2017.csv', col_names = F, trim_ws = T)
+
+laptops <- c(
+  "LG Touchscreen Laptop",
+  "Acer Aspire",
+  "HP Laptop",
+  "ASUS Chromebook",
+  "Apple MacBook Pro",
+  "Apple MacBook Air",
+  "Dell Laptop",
+  "Eluktronics Pro Gaming Laptop",
+  "Alienware Laptop",
+  "HP Notebook Touchscreen Laptop PC"
+  )
+
+desktops <- c(
+  "Lenovo Desktop Computer",
+  #"iMac",
+  "HP Desktop",
+  "ASUS Desktop",
+  "Dell Desktop",
+  "Intel Desktop",
+  "Acer Desktop",
+  "CYBERPOWER Gamer Desktop"
+  )
+
+monitors <- c(
+  "Acer Monitor",
+  "LG Monitor",
+  "ASUS Monitor",
+  "ASUS 2 Monitor",
+  "Dell Monitor",
+  "Samsung Monitor",
+  "Sceptre Monitor",
+  "ViewSonic Monitor",
+  "AOC Monitor",
+  "HP Monitor"
+  )
+
+iodevices <- c(
+  "3-Button Mouse",
+  "Logitech Wireless Mouse",
+  "Microsoft Basic Optical Mouse",
+  "Logitech 3-button Mouse",
+  "Redragon Gaming Mouse  ",
+  "HP Wireless Mouse",
+  "Generic Black 3-Button",
+  "Wireless Portable Mouse",
+  "Gaming Mouse Professional",
+  "Slim Wireless Mouse",
+  "HP USB Keyboard",
+  "Logitech Wireless Keyboard",
+  "Rii LED Keyboard",
+  "Logitech Keyboard",
+  "Backlit LED Gaming Keyboard",
+  "Dell Wired Keyboard",
+  "Apple Wired Keyboard",
+  "Apple Wireless Keyboard",
+  "Apple Magic Keyboard",
+  "Logitech MK550 Wireless Wave Keyboard and Mouse Combo",
+  "Logitech Desktop MK120 Mouse and keyboard Combo",
+  "Logitech MK270 Wireless Keyboard and Mouse Combo",
+  "Dell KM117 Wireless Keyboard & Mouse",
+  "EagleTec Wireless Combo Keyboard and Mouse",
+  "Microsoft Wireless Comfort Keyboard and Mouse",
+  "Microsoft Wireless Desktop Keyboard and Mouse",
+  "Rii LED Gaming Keyboard & Mouse Combo",
+  "Logitech MK360 Wireless Keyboard and Mouse Combo"
+)
+
+for (i in seq_along(laptops)){
+  df[df == laptops[i]] <- "Laptop"
+}
+
+for (i in seq_along(desktops)){
+  df[df == desktops[i]] <- "Desktop"
+}
+
+for (i in seq_along(monitors)){
+  df[df == monitors[i]] <- "Monitor"
+}
+
+for (i in seq_along(iodevices)){
+  df[df == iodevices[i]] <- "I/O-device"
+}
+
+readr::write_csv(df, './input/replaced.csv', na="", col_names = F)
--- week6/R/transactions.R
+++ week6/R/transactions.R
@@ -1,16 +1,18 @@
-## Multiple regression - week 6
+## Market basket analysis - week 6
 ## Toine - March 2019
 
 ## Load libraries =================================
 library(arulesViz)
 library(arules)
 
+
 # # Prepare clusters =================================
 # cl <- makeCluster(3)
 # registerDoParallel(cl)
 
 ## Import dataset =================================
-elecTransactions <- read.transactions('./input/ElectronidexTransactions2017.csv', sep = ',', rm.duplicates = F, format = "basket")
+source('./R/replacer.R')
+elecTransactions <- read.transactions('./input/replaced.csv', sep = ',', rm.duplicates = F, format = "basket")
 
 ## Explore dataset =================================
 length(elecTransactions) #provides total amount of observations
@@ -45,3 +47,35 @@ mean(size(elecTransactions))
 
 #Visualization of sample of transactions
 image(sample(elecTransactions, 200))
+
+## Building of the model ====================================
+rules <- apriori(elecTransactions, 
+                 parameter = list(support = 0.05, confidence = 0.5, minlen = 2)
+                )
+inspect(sort(rules, by = "lift"))
+plot(rules, method = "graph", shading = NA)
+
+
+## Digging into Laptops =====================================
+rules1 <- apriori(elecTransactions, 
+                 parameter = list(support = 0.01, confidence = 0.2, minlen = 2),
+                 appearance = list(default="rhs", lhs="Laptop")
+                  )
+inspect(sort(rules1, by = "lift"))
+plot(rules1, method = "graph", shading = NA)
+
+## Digging into Desktops =====================================
+rules1 <- apriori(elecTransactions, 
+                  parameter = list(support = 0.01, confidence = 0.2, minlen = 2),
+                  appearance = list(default="rhs", lhs="Desktop")
+)
+inspect(sort(rules1, by = "lift"))
+plot(rules1, method = "graph", shading = NA)
+
+## Digging into Monitors =====================================
+rules1 <- apriori(elecTransactions, 
+                  parameter = list(support = 0.01, confidence = 0.2, minlen = 2),
+                  appearance = list(default="rhs", lhs="Laptop")
+)
+inspect(sort(rules1, by = "lift"))
+plot(rules1, method = "graph", shading = NA)
