5249056036a9dbaf5a50dd7d16dccc394ee64c9d
NYX <nyx@tano.bytano.nl>
2019-05-17 14:05:11 +0200

    add scripts

---
+++ mushrooms/R/data_exploration.R
@@ -0,0 +1,60 @@
+
+# Load libraries ----------------------------------------------------------
+library(tidyverse)
+library(magrittr)
+library(caret)
+library(doParallel)
+
+# Load data ---------------------------------------------------------------
+mushData <- read_rds('./input/train.rds')
+mushData %<>% select(-c("odor", contains("veil"), gill.attachment, population, habitat))
+
+#veil.color - not very much difference
+#veil.
+#important: bruises, stalk, ring, spore
+#Investigate whether gill.size and gill.color is available in verification data
+
+
+table(mushData$class, mushData$gill.attachment)
+
+
+#create weights matrix
+matrix_weights <- list(c("e", "p"), c("e", "p"))
+names(matrix_weights) <- c("predicted", "actual")
+error_cost <- matrix(c(0, 1, 4, 0), nrow = 2, dimnames = matrix_weights)
+
+cl <- makeCluster(detectCores() - 1)
+registerDoParallel(cl)
+
+set.seed(541)
+# train and test
+train_ids <- createDataPartition(y = mushData$class,
+                                 p = 0.75,
+                                 list = F)
+train <- mushData[train_ids,]
+test <- mushData[-train_ids,]
+
+# cross validation
+ctrl <- trainControl(method = "repeatedcv",
+                     number = 4,
+                     repeats = 1,
+                     summaryFunction = twoClassSummary,
+                     classProbs = TRUE
+)
+
+rfFit1 <- caret::train(class~ .,
+                       data = train,
+                       method = "rpart",
+                       trControl=ctrl,
+                       metric = "ROC"
+                       #preProcess = c("scale", "center") #only used for distance-modelling techniques (knn, SVM)
+)
+
+# Stop Cluster. 
+stopCluster(cl)
+
+# Predicting testset ================================
+test$Predictions <- predict(rfFit1, test)
+postResample(factor(test$Predictions), factor(test$class))
+
+confusionMatrix(data = factor(test$Predictions), reference = factor(test$class))
---
+++ mushrooms/R/rpart.R
@@ -0,0 +1,86 @@
+
+# Load libraries ----------------------------------------------------------
+library(tidyverse)
+library(magrittr)
+library(caret)
+library(doParallel)
+
+# Load data ---------------------------------------------------------------
+mushData <- read_rds('./input/train.rds')
+mushData %<>% select(-c("odor", contains("veil"), contains("gill"), ring.type, cap.surface, population, habitat))
+mushData %<>% select(
+  c(
+    "class",
+    "cap.color",
+    "ring.number",
+    "spore.print.color",
+    "stalk.shape",
+    "stalk.root",
+    "bruises"
+  )
+)
+
+#possible addback: ring.type
+
+#important: bruises, stalk, ring, spore
+#Investigate whether gill.size and gill.color is available in verification data
+
+table(mushData$class, mushData$stalk.root, exclude = NULL)
+
+set.seed(541)
+# train and test
+train_ids <- createDataPartition(y = mushData$class,
+                                 p = 0.75,
+                                 list = F)
+train <- mushData[train_ids, ]
+test <- mushData[-train_ids, ]
+
+fit <- rpart(
+  class ~ .,
+  data = mushData,
+  method = "class",
+  parms = list(split = "information", loss = matrix(
+    c(0, 3, 1, 0), byrow = TRUE, nrow = 2
+  )),
+  control = rpart.control(usesurrogate = 0, maxsurrogate = 0)
+)
+
+
+# Predicting testset ================================
+model <- fit
+test$Predictions <- predict(model, test)
+test %<>% mutate(Predictions = ifelse(Predictions[, "e"] == 1, "e", "p"))
+
+postResample(factor(test$Predictions), factor(test$class))
+
+confusionMatrix(data = factor(test$Predictions),
+                reference = factor(test$class))
+
+fit2 <-
+  prune(fit, cp = fit$cptable[which.min(fit$cptable[, "xerror"]), "CP"])
+
+save(fit, file = './output/modelToine2.rds')
+
+library(rattle)
+library(rpart.plot)
+library(RColorBrewer)
+
+fancyRpartPlot(fit2)
+library(mlr)
+train2 <- train %>% 
+  mutate(
+  class = factor(class),
+  cap.color = factor(cap.color),
+  ring.number = factor(ring.number),
+  spore.print.color = factor(spore.print.color),
+  stalk.shape = factor(stalk.shape),
+  stalk.root = factor(stalk.root),
+  bruises = factor(bruises)
+  )
+classif.task = makeClassifTask(data = train2, target = "class", positive = "e")
+classif.lrn = makeLearner("classif.randomForest", predict.type = "prob", fix.factors.prediction = TRUE)
+mod = train(classif.lrn, classif.task)
+task.pred = predict(mod, newdata = test2)
+calculateConfusionMatrix(task.pred)
+
+save(mod, file = './output/modelToine3.rds')
